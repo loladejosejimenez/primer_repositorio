298fe1e303fea82ff060a65fda78ff3f42edf83b
loladejosejimenez <lola-dj@hotmail.com>
2014-09-08 19:56:10 +0200

    Primer Ejercicio de JavaScript
    Inicio Primer Ejercicio JavaScript Lunes 8 de septiembre de 2014.

---
+++ js/scripts.js
@@ -0,0 +1,50 @@
+/* Comentar
+en 
+bloque
+*/
+
+// Comentario de linea
+
+//Prueba de funcionamiento de JavaScript
+//document.write("Si funciona mi JavaScript");
+
+//Generar alerta a usuario
+//alert("Ventana de alerta para usuario");
+
+//Solicitar información a nuestro usuario
+/*
+var nombre = prompt("Por favor introduzca su nombre");
+document.write("<h1>Bienvenid@: " + nombre + "</h1><br />");
+
+var edad = prompt("Por favor introduzca su edad");
+document.write("Su edad es: " + edad + "años");
+
+*/
+
+//Operaciones matemáticas
+/*
+var numUno = 5;
+var numDos = 2;
+var numTres = 2;
+
+var suma = numUno + numDos;
+
+var resultado = suma / numTres;
+console.log(suma);
+
+console.log(resultado);
+*/
+
+
+//Operaciones matemáticas personalizadas
+
+var numUno = parseInt(prompt("Introduzca Primer Valor"));
+var numDos = parseInt(prompt("Introduzca Segundo Valor"));
+var numTres = parseInt(prompt("Introduzca Tercer Valor"));
+
+var resultado = (numUno + numDos) / numTres;
+
+console.log("La operación es la siguiente: " + numUno + " + " + numDos + " / " + numTres);
+
+console.log(resultado);
+
